f95da902e39465d265f00d5dec544819d25c57b0
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-09-23 20:37:59 -0300

    feat: set route to create classes

--- server/src/database/migrations/02_create_class_schedule.ts
+++ server/src/database/migrations/02_create_class_schedule.ts
@@ -1,15 +1,11 @@
 import Knex from 'knex';
 
 export async function up(knex: Knex) {
-  return knex.schema.createTable('class_schedule', table => {
+  return knex.schema.createTable('class_schedule', (table) => {
     table.increments('id').primary();
-
     table.integer('week_day').notNullable();
     table.integer('from').notNullable();
     table.integer('to').notNullable();
-
-    table.string('subject').notNullable();
-    table.decimal('cost').notNullable();
 
     table.integer('class_id')
       .notNullable()
