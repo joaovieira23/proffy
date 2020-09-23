b8651d19133d0760aa145592f59ee3290bffe461
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-09-23 15:44:50 -0300

    chore: configure and create tables in the database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello World")
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+})
+
+app.listen(3333);
