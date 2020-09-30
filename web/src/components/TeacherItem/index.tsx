44a91d9a77dac16725c638f948733e7d93a74f60
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-09-30 01:26:49 -0300

    feat: add contact and number of connections

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 import './styles.css';
 
 export interface Teacher {
@@ -17,7 +18,13 @@ interface TeacherItemProps {
   teacher: Teacher;
 }
 
+
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id,
+    })
+  }
   return (
     <article className="teacher-item">
       <header>
@@ -36,10 +43,14 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
           Preço/Hora
               <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          target="_blank"
+          onClick={createNewConnection}
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
+            </a>
       </footer>
     </article>
   );
